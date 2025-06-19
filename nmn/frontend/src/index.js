6176ff254c973790fd95b133a031f42c6d16ab54
tharunkumarvk <rustyboffinbot@gmail.com>
2025-06-19 08:15:38 +0530

    Fully verified

--- nmn/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "@fontsource/poppins"; // For modern font
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
